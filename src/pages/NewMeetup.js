16b52a6d28228577df97c577baa14a8875f80a4d
BILETALU <[ivanovv.tca@gmail.com]>
2021-09-20 22:11:57 +0600

    reworked react hook useHystory in newMeetup page

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,17 +1,19 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function AddMeetupHandler(meetupData) {
-  const history = useHistory();
-  fetch("https://meetups-3cbbb-default-rtdb.firebaseio.com/meetups.json", {
-    method: "POST",
-    body: JSON.stringify(meetupData),
-    headers: { "Content-Type": "application/json" },
-  }).then(() => {
-    history.replace("/");
-  });
-}
 function NewMeetup() {
+  const history = useHistory();
+  function AddMeetupHandler(meetupData) {
+    // console.log(history)
+    fetch("https://meetups-3cbbb-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: { "Content-Type": "application/json" },
+    }).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <div>
       <NewMeetupForm onAddMeetup={AddMeetupHandler} />
