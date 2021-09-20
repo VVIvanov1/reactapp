669d47ae1adc24c970877a5da808fd657980f767
= <=>
2021-09-20 15:33:47 +0600

    add form submission procedure

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,37 @@
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+import { useRef } from "react";
+//https://meetups-3cbbb-default-rtdb.firebaseio.com/
+
+function NewMeetupForm(props) {
+  const titleInputRef =  useRef();
+  const descrInputRef = useRef()
+  function submitHandler(e) {
+    e.preventDefault();
+    const enteredData = {
+        title: titleInputRef.current.value,
+        description: descrInputRef.current.value
+    }
+    // console.log(enteredData);
+    props.onAddMeetup(enteredData)
+  }
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea required id="description" rows="5" ref={descrInputRef}/>
+        </div>
+        <div className={classes.action}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,22 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
+
+function AddMeetupHandler(meetupData) {
+  const history = useHistory();
+  fetch("https://meetups-3cbbb-default-rtdb.firebaseio.com/meetups.json", {
+    method: "POST",
+    body: JSON.stringify(meetupData),
+    headers: { "Content-Type": "application/json" },
+  }).then(() => {
+    history.replace("/");
+  });
+}
 function NewMeetup() {
-  return <div>NewMeetup Page</div>;
+  return (
+    <div>
+      <NewMeetupForm onAddMeetup={AddMeetupHandler} />
+    </div>
+  );
 }
 
 export default NewMeetup;
